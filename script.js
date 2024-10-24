daaee8af6c8a129ad27bff40f62040a3a8a80093
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-24 13:16:28 +0100

    makeCalculator() function created and implemented
    Function in JS creates the required buttons for the calculator face,
    and has grouped them in classes relevant to the content of each button.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 const calculator = document.querySelector("calculator");
-const calculatorButtons = document.querySelector(".calculator_btns");
+const calculatorButtons = document.querySelector(".calculator__btns");
+const buttons = ["%", "C", "DEL", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "±", "0", ".", "="]
+const totalButtons = buttons.length;
 
 function makeCalculator() {
-    
+    for (let x = 0; x < totalButtons; x++){
+        let calculatorBtn = document.createElement("btn");
+        calculatorBtn.textContent = buttons[x];
+        calculatorBtn.style.fontSize = "2em";
+        if (calculatorBtn.textContent == "÷" ||
+            calculatorBtn.textContent == "+" ||
+            calculatorBtn.textContent == "-" ||
+            calculatorBtn.textContent == "x" ||
+            calculatorBtn.textContent == "="){
+                calculatorBtn.classList.add("calculator__btns__op");
+                calculatorBtn.style.backgroundColor = "salmon";
+        }
+        else if (calculatorBtn.textContent == "%" ||
+                 calculatorBtn.textContent == "±"){
+                    calculatorBtn.classList.add("calculator__btns__op");
+                    calculatorBtn.style.backgroundColor = "salmon";
+        }
+        else if (calculatorBtn.textContent == "C" ||
+                 calculatorBtn.textContent == "DEL"){
+                    calculatorBtn.classList.add("calculator__btns__clear");
+                    calculatorBtn.style.backgroundColor = "salmon";
+        }
+        else if (calculatorBtn.textContent == "."){
+                   calculatorBtn.classList.add("calculator__btns__dot");
+                   calculatorBtn.style.backgroundColor = "salmon";
+        }
+        else{
+            calculatorBtn.classList.add("calculator__btns__num");
+        }
+                 
+        calculatorButtons.appendChild(calculatorBtn);
+    }
+    calculator.appendChild(calculatorButtons);
 }
+
+makeCalculator();

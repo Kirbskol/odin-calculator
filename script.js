955052936c38680f717b8af0108a602b29f720bc
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-24 16:14:49 +0100

    Change loop to generate buttons as issue previously now patched
    The way the buttons were previously generated prevented any operation
    being completed outside of the loop. This has now been changed to
    include an element listener, which can be used externally in functions,
    in order to be able to analyse and use input from buttons.

--- script.js
+++ script.js
@@ -1,51 +1,39 @@
 const calculator = document.querySelector("calculator");
 const calculatorButtons = document.querySelector(".calculator__btns");
-const buttons = ["%", "C", "DEL", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "±", "0", ".", "="]
-let calculatorBtn = [];
-const totalButtons = buttons.length;
-let userInput = [];
+const calculatorResult = document.querySelector("#calculator__screen__result");
+const allButtons = ["%", "C", "DEL", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "±", "0", ".", "="];
+const allFunctions = ["%", ".", "=", "÷", "-", "x", "C", "DEL", "±", "+"];
+const allOperators = ["%", "=", "x", "-", "÷", "+", "±"];
+let selectedNums = [];
+let selectedOps = [];
 
-function makeCalculator() {
-    for (let x = 0; x < totalButtons; x++){
-        calculatorBtn[x] = document.createElement("btn");
-        calculatorBtn[x].textContent = buttons[x];
-        calculatorBtn[x].style.fontSize = "2em";
-        if (calculatorBtn[x].textContent == "÷" ||
-            calculatorBtn[x].textContent == "+" ||
-            calculatorBtn[x].textContent == "-" ||
-            calculatorBtn[x].textContent == "x" ||
-            calculatorBtn[x].textContent == "="){
-                calculatorBtn[x].classList.add("calculator__btns__op");
-                calculatorBtn[x].style.backgroundColor = "salmon";
-        }
-        else if (calculatorBtn[x].textContent == "%" ||
-                 calculatorBtn[x].textContent == "±"){
-                    calculatorBtn[x].classList.add("calculator__btns__op");
-                    calculatorBtn[x].style.backgroundColor = "salmon";
-        }
-        else if (calculatorBtn[x].textContent == "C" ||
-                 calculatorBtn[x].textContent == "DEL"){
-                    calculatorBtn[x].classList.add("calculator__btns__clear");
-                    calculatorBtn[x].style.backgroundColor = "salmon";
-        }
-        else if (calculatorBtn[x].textContent == "."){
-                   calculatorBtn[x].classList.add("calculator__btns__dot");
-                   calculatorBtn[x].style.backgroundColor = "salmon";
-        }
-        else{
-            calculatorBtn[x].classList.add("calculator__btns__num");
-        }
-        calculatorBtn[x].classList.add("calculator__btns__all");
-        calculatorBtn[x].addEventListener("click", getInput);
-        function getInput(){
-            let buttonContent = calculatorBtn[x].textContent;
-            userInput.push(`${buttonContent}`);
-        }
-        calculatorButtons.appendChild(calculatorBtn[x]);
-    }
-    calculator.appendChild(calculatorButtons);
+const makeCalculator = () => {
+    allButtons.forEach((element) => {
+        const singleButton = document.createElement("button");
+        singleButton.textContent = element;
+        singleButton.style.fontSize = "2em";
+        singleButton.classList.add("calculator__btns__all");
+        if (allFunctions.includes(element)) { 
+                                                singleButton.setAttribute("style", 
+                                                                          "background-color: salmon") 
+                                            }
+        singleButton.addEventListener("click", () => getButtonInput(element.toString()));
+        calculatorButtons.appendChild(singleButton);
+    });
 }
 
-
+const getButtonInput = (element) => {
+    if (Number(element) >= 0 ){
+        calculatorResult.value += element;
+        selectedNums.push(element);
+    }
+    else if (element === "+" ||
+             element === "-" ||
+             element === "x" ||
+             element === "÷" ||
+             element === "%" ||
+             element === "±")
+        selectedOps.push(element);
+}
 
 makeCalculator();

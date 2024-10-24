aa9e1fee171429f14b477a4ea33dafbadbe1d457
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-24 14:34:34 +0100

    Add eventListener to buttons to store text content in variable
    When buttons are pressed, their text content is stored in a global
    array.

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 const calculator = document.querySelector("calculator");
 const calculatorButtons = document.querySelector(".calculator__btns");
 const buttons = ["%", "C", "DEL", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "±", "0", ".", "="]
+let calculatorBtn = [];
 const totalButtons = buttons.length;
+let userInput = [];
 
 function makeCalculator() {
     for (let x = 0; x < totalButtons; x++){
-        let calculatorBtn = document.createElement("btn");
-        calculatorBtn.textContent = buttons[x];
-        calculatorBtn.style.fontSize = "2em";
-        if (calculatorBtn.textContent == "÷" ||
-            calculatorBtn.textContent == "+" ||
-            calculatorBtn.textContent == "-" ||
-            calculatorBtn.textContent == "x" ||
-            calculatorBtn.textContent == "="){
-                calculatorBtn.classList.add("calculator__btns__op");
-                calculatorBtn.style.backgroundColor = "salmon";
+        calculatorBtn[x] = document.createElement("btn");
+        calculatorBtn[x].textContent = buttons[x];
+        calculatorBtn[x].style.fontSize = "2em";
+        if (calculatorBtn[x].textContent == "÷" ||
+            calculatorBtn[x].textContent == "+" ||
+            calculatorBtn[x].textContent == "-" ||
+            calculatorBtn[x].textContent == "x" ||
+            calculatorBtn[x].textContent == "="){
+                calculatorBtn[x].classList.add("calculator__btns__op");
+                calculatorBtn[x].style.backgroundColor = "salmon";
         }
-        else if (calculatorBtn.textContent == "%" ||
-                 calculatorBtn.textContent == "±"){
-                    calculatorBtn.classList.add("calculator__btns__op");
-                    calculatorBtn.style.backgroundColor = "salmon";
+        else if (calculatorBtn[x].textContent == "%" ||
+                 calculatorBtn[x].textContent == "±"){
+                    calculatorBtn[x].classList.add("calculator__btns__op");
+                    calculatorBtn[x].style.backgroundColor = "salmon";
         }
-        else if (calculatorBtn.textContent == "C" ||
-                 calculatorBtn.textContent == "DEL"){
-                    calculatorBtn.classList.add("calculator__btns__clear");
-                    calculatorBtn.style.backgroundColor = "salmon";
+        else if (calculatorBtn[x].textContent == "C" ||
+                 calculatorBtn[x].textContent == "DEL"){
+                    calculatorBtn[x].classList.add("calculator__btns__clear");
+                    calculatorBtn[x].style.backgroundColor = "salmon";
         }
-        else if (calculatorBtn.textContent == "."){
-                   calculatorBtn.classList.add("calculator__btns__dot");
-                   calculatorBtn.style.backgroundColor = "salmon";
+        else if (calculatorBtn[x].textContent == "."){
+                   calculatorBtn[x].classList.add("calculator__btns__dot");
+                   calculatorBtn[x].style.backgroundColor = "salmon";
         }
         else{
-            calculatorBtn.classList.add("calculator__btns__num");
+            calculatorBtn[x].classList.add("calculator__btns__num");
         }
-                 
-        calculatorButtons.appendChild(calculatorBtn);
+        calculatorBtn[x].classList.add("calculator__btns__all");
+        calculatorBtn[x].addEventListener("click", getInput);
+        function getInput(){
+            let buttonContent = calculatorBtn[x].textContent;
+            userInput.push(`${buttonContent}`);
+        }
+        calculatorButtons.appendChild(calculatorBtn[x]);
     }
     calculator.appendChild(calculatorButtons);
 }
+
+
 
 makeCalculator();
